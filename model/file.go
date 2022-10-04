725e26e90f1d4b3b833aaecfabb3c65b2b92f254
Mario Seidel <mario.seidel@dmk-ebusiness.com>
2022-10-04 10:10:40 +0200

    add check for symlink

--- model/file.go
+++ model/file.go
@@ -24,7 +24,6 @@ func NewFile(p string) (*File, error) {
 	}, nil
 }
 
-
 func (file File) Hash() ([]byte, error) {
 	f, err := os.Open(file.Path)
 	defer f.Close()
@@ -42,12 +41,22 @@ func (file File) Hash() ([]byte, error) {
 	return h.Sum(nil), nil
 }
 
-func (f File) Info() (*os.FileInfo, error) {
-	s, err := os.Stat(f.Path)
+func (file File) Info() (*os.FileInfo, error) {
+	s, err := os.Stat(file.Path)
 
 	if err != nil {
 		return nil, err
 	}
 
 	return &s, err
+}
+
+func (file File) IsSymlink() (bool, error) {
+	fileInfo, err := file.Info()
+
+	if err != nil {
+		return false, err
+	}
+
+	return (*fileInfo).Mode()&os.ModeSymlink == os.ModeSymlink, nil
 }
